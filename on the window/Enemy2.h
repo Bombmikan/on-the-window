913c4acc0011a88c166db1940f3a44c3cf79ff9c
DESKTOP-GTVOAS2\G2439 <yukiyajin183@yahoo.co.jp>
2025-12-11 12:37:58 +0900

    クラス作成、コンストラクタまで

--- on the window/Enemy2.h
+++ on the window/Enemy2.h
@@ -1,1 +1,20 @@
-#pragma once
+﻿#pragma once
+#include <cmath>
+
+class Enemy2
+{
+private:
+	Vec2 m_pos;
+	Vec2 m_vel;
+	double Radius = 100.0;
+
+	//挙動パラメータ
+	double maxSpeed = 100.0;	//最大速度
+	double maxAccel = 140.0;	//最大加速度
+	double distance = 10.0;
+public:
+	Enemy2(const Vec2& startPos)
+		: m_pos(startPos)
+		, m_vel(0, 0)
+	{ }
+};
