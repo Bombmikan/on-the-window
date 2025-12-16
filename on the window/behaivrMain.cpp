ad028f4699c0392cdab64dfdb4c79982956ae9ce
DESKTOP-GTVOAS2\G2439 <yukiyajin183@yahoo.co.jp>
2025-12-16 11:22:44 +0900

    behaivr機能構築

---
+++ on the window/behaivrMain.cpp
@@ -0,0 +1,21 @@
+﻿#include "beheaivrTree.h"
+# include <Siv3D.hpp> // Siv3D v0.6.16 (描画や入力、時間管理のために利用します)
+
+using namespace std;
+void Main() {
+	//ツリーの構築
+	auto root = make_shared<Selector>();
+
+	auto seq = make_shared<Sequence>();
+	seq->addChild(make_shared<CheckHealth>(20));
+	seq->addChild(make_shared<PrintLog>(U"Health is low!"));
+
+	root->addChild(seq);
+	root->addChild(make_shared<PrintLog>(U"Normal state"));
+
+	while (System::Update()) {
+		//マイフレーム
+		root->update();
+	}
+	
+}
